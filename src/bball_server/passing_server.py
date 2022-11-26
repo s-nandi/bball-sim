e46948e1e628c00f419095fe6d1f7279a4388703
s-nandi <saujas.nandi@gmail.com>
2022-11-26 09:19:42 -0500

    Update passing to account for movement

--- src/bball_server/passing_server.py
+++ src/bball_server/passing_server.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import Optional
+import pymunk
 from bball_server.player import Player
+from bball_server.utils import convert_to_tuple
 
 
 @dataclass
@@ -13,32 +15,45 @@ class PassPlayers:
 class PassingServer:
     _has_active_pass: bool
     _players_involved: Optional[PassPlayers]
-    _steps_to_complete_pass: float
+    _pass_velocity: float
+    _original_position: pymunk.Vec2d
+    _time_since_pass: float
 
     def __init__(self):
-        self._has_active_pass = False
-        self._players_involved = None
-        self._steps_to_complete_pass = 0
+        self._reset()
 
     def pass_between(
-        self, passer: Player, receiver: Player, complete_in: float
+        self, passer: Player, receiver: Player, pass_velocity: float
     ) -> PassingServer:
         assert not self._has_active_pass
-        assert complete_in > 0
+        assert pass_velocity > 0
         self._has_active_pass = True
         self._players_involved = PassPlayers(passer, receiver)
-        self._steps_to_complete_pass = complete_in
+        self._pass_velocity = pass_velocity
+        self._original_position = pymunk.Vec2d(*passer.position)
         return self
 
-    def _complete_pass(self):
+    def _complete_pass(self) -> None:
         assert self._has_active_pass
         self._players_involved.receiver.give_ball()
         self._players_involved.passer.give_up_ball()
+        self._reset()
+
+    def _reset(self) -> None:
         self._has_active_pass = False
+        self._players_involved = None
+        self._original_position = pymunk.Vec2d(0, 0)
+        self._pass_velocity = 0
+        self._has_active_pass = False
+        self._time_since_pass = 0
 
     def _step(self, time_step: float) -> PassingServer:
-        if self._has_active_pass:
-            self._steps_to_complete_pass -= time_step
-            if self._steps_to_complete_pass <= 0:
-                self._complete_pass()
+        if not self._has_active_pass:
+            return self
+        self._time_since_pass += time_step
+        receiver_position = self._players_involved.receiver.position
+        distance = self._original_position.get_distance(receiver_position)
+        covered = self._pass_velocity * self._time_since_pass
+        if covered >= distance:
+            self._complete_pass()
         return self
