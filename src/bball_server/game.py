60fe766e08adba6d7a22e10c1d09e2cb04fe21fb
s-nandi <saujas.nandi@gmail.com>
2022-11-28 00:49:26 -0800

    Remove more dead code, add hoop to sketch in test

--- src/bball_server/game.py
+++
@@ -1,9 +0,0 @@
-from bball_server.player.player_server import PlayerServer
-
-
-class Game:
-    def __init__(self, _player_server: PlayerServer):
-        pass
-
-    def create_player(self):
-        pass
--- src/bball_server/player/__init__.py
+++ src/bball_server/player/__init__.py
@@ -1,3 +1,2 @@
 from .player import Player
 from .player_attributes import PlayerAttributes
-from .player_server import PlayerServer
