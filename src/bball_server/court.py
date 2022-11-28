ef11f025b4580da9d55bbd97a61326944ad2008d
s-nandi <saujas.nandi@gmail.com>
2022-11-27 22:36:49 -0500

    Tweak court bounds checking

--- src/bball_server/court.py
+++ src/bball_server/court.py
@@ -5,15 +5,14 @@ from bball_server.player import Player
 
 
 class Court:
-    _width: float
-    _height: float
+    _dimensions: Tuple[float, float]
 
     def __init__(self, dimensions: Tuple[float, float]):
-        self._width = dimensions[0]
-        self._height = dimensions[1]
+        self._dimensions = dimensions
 
     def is_inbounds(self, player: Player) -> bool:
         position = player.position
-        x_in_bounds = 1 <= position[0] <= self._width
-        y_in_bounds = 1 <= position[1] <= self._height
-        return x_in_bounds and y_in_bounds
+        inbounds = True
+        for dimension in range(2):
+            inbounds &= 1 <= position[dimension] <= self._dimensions[dimension]
+        return inbounds
