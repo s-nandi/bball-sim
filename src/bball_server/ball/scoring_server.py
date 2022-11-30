035d2e34c0c551a630441c28cfacf216574a8b18
s-nandi <saujas.nandi@gmail.com>
2022-11-29 22:39:57 -0800

    Use dataclass for scoring server

--- src/bball_server/ball/scoring_server.py
+++ src/bball_server/ball/scoring_server.py
@@ -1,13 +1,10 @@
+from dataclasses import dataclass
 from bball_server.player import Player
 from bball_server.utils import Point
 
 
+@dataclass
 class _ScoringServer:
     _shot_by: Player
     _shot_at: Point
     _shot_from: Point
-
-    def __init__(self, shooter: Player, target: Point, shot_from: Point):
-        self._shot_by = shooter
-        self._shot_at = target
-        self._shot_from = shot_from
