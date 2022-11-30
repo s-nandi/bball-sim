e339697c658e1e288fa5da4a5990645e02064a83
s-nandi <saujas.nandi@gmail.com>
2022-11-29 23:52:21 -0800

    Add held ball server

--- src/bball_server/ball/ball.py
+++ src/bball_server/ball/ball.py
@@ -7,6 +7,8 @@ from bball_server.ball.shooting_server import _ShootingServer
 from bball_server.ball.scoring_server import _ScoringServer
 from bball_server.ball.dead_ball_server import _DeadBallServer
 from bball_server.ball.post_pass_server import _PostPassServer
+from bball_server.ball.held_ball_server import _HeldBallServer
+
 
 if TYPE_CHECKING:
     from bball_server.player import Player
@@ -22,7 +24,12 @@ class BallMode(Enum):
 
 
 _Server = Union[
-    _PassingServer, _ShootingServer, _ScoringServer, _DeadBallServer, _PostPassServer
+    _PassingServer,
+    _ShootingServer,
+    _ScoringServer,
+    _DeadBallServer,
+    _PostPassServer,
+    _HeldBallServer,
 ]
 
 
@@ -35,7 +42,6 @@ class Ball:
 
     def __init__(self):
         self._position = (0, 0)
-        self._belongs_to = None
         self._last_belonged_to = None
         self._server = _DeadBallServer(False)
         self._mode = BallMode.DEAD
@@ -78,38 +84,32 @@ class Ball:
         assert isinstance(server, _PostPassServer)
         return server._receiver
 
-    def _unsafe_belongs_to(self) -> Player:
-        assert self._belongs_to is not None
-        return self._belongs_to
+    @property
+    def belongs_to(self) -> Player:
+        server = self._unsafe_server()
+        assert isinstance(server, _HeldBallServer)
+        return server._ball_handler
 
     def _unsafe_server(self) -> _Server:
         assert self._server is not None
         return self._server
 
-    def _give_posession(self, player: Player) -> None:
-        self._belongs_to = player
-        self._unsafe_belongs_to()._ball = self
-        self._last_belonged_to = player
-
-    def _remove_posession(self) -> None:
-        self._unsafe_belongs_to()._ball = None
-        self._belongs_to = None
-
     def _step(self, time_frame: float) -> bool:
         if self._mode in [BallMode.POSTPASS, BallMode.POSTSHOT, BallMode.DEAD]:
             return False
-        if self._mode == BallMode.HELD:
-            self._position = self._unsafe_belongs_to().position
-            return False
-        if self._mode in [BallMode.MIDPASS, BallMode.MIDSHOT]:
+        if self._mode in [BallMode.HELD, BallMode.MIDPASS, BallMode.MIDSHOT]:
             server = self._unsafe_server()
-            assert isinstance(server, (_ShootingServer, _PassingServer))
+            assert isinstance(
+                server, (_ShootingServer, _PassingServer, _HeldBallServer)
+            )
             return server._step(time_frame)
         assert False, f"Invalid ball mode {self._mode}"
 
     def _reset(self) -> None:
         if self._mode == BallMode.HELD:
-            self._remove_posession()
+            server = self._unsafe_server()
+            assert isinstance(server, _HeldBallServer)
+            server._reset()
         elif self._mode in [
             BallMode.MIDPASS,
             BallMode.MIDSHOT,
@@ -117,18 +117,13 @@ class Ball:
             BallMode.DEAD,
             BallMode.POSTPASS,
         ]:
-            self._server = None
+            pass
         else:
             assert False
 
     def _with_mode(self, mode: BallMode) -> Ball:
         self._mode = mode
         return self
-
-    def _give_to(self, player: Player) -> Ball:
-        self._position = player.position
-        self._give_posession(player)
-        return self._with_mode(BallMode.HELD)
 
     def held_out_of_bounds(self):
         assert self._mode == BallMode.HELD
@@ -138,10 +133,15 @@ class Ball:
 
     def pass_to(self, receiver: Player, pass_velocity: float) -> Ball:
         assert self._mode == BallMode.HELD
-        passer = self._unsafe_belongs_to()
         self._reset()
-        self._server = _PassingServer(passer, receiver, self, pass_velocity)
+        self._server = _PassingServer(self.belongs_to, receiver, self, pass_velocity)
         return self._with_mode(BallMode.MIDPASS)
+
+    def shoot_at(self, target: Point, shot_velocity: float) -> Ball:
+        assert self._mode == BallMode.HELD
+        self._reset()
+        self._server = _ShootingServer(self.belongs_to, target, self, shot_velocity)
+        return self._with_mode(BallMode.MIDSHOT)
 
     def post_pass(self, receiver: Player) -> Ball:
         assert self._mode == BallMode.MIDPASS
@@ -152,14 +152,8 @@ class Ball:
     def successful_pass(self, receiver: Player) -> Ball:
         assert self._mode == BallMode.POSTPASS
         self._reset()
-        return self._give_to(receiver)
-
-    def shoot_at(self, target: Point, shot_velocity: float) -> Ball:
-        assert self._mode == BallMode.HELD
-        shooter = self._unsafe_belongs_to()
-        self._reset()
-        self._server = _ShootingServer(shooter, target, self, shot_velocity)
-        return self._with_mode(BallMode.MIDSHOT)
+        self._server = _HeldBallServer(receiver, self)
+        return self._with_mode(BallMode.HELD)
 
     def post_shot(self, shooter: Player, target: Point, shot_from: Point) -> Ball:
         assert self._mode == BallMode.MIDSHOT
@@ -176,4 +170,5 @@ class Ball:
     def jump_ball_won_by(self, receiver: Player) -> Ball:
         assert self._mode == BallMode.DEAD
         self._reset()
-        return self._give_to(receiver)
+        self._server = _HeldBallServer(receiver, self)
+        return self._with_mode(BallMode.HELD)
---
+++ src/bball_server/ball/held_ball_server.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bball_server.player import Player
+    from bball_server.ball import Ball
+
+
+class _HeldBallServer:
+    _ball_handler: Player
+    _ball: Ball
+
+    def __init__(self, receiver: Player, ball: Ball):
+        self._ball_handler = receiver
+        self._ball = ball
+        receiver._ball = ball
+        ball._last_belonged_to = receiver
+        ball._position = receiver.position
+
+    def _step(self, _time_frame: float) -> bool:
+        self._ball._position = self._ball_handler.position
+        return False
+
+    def _reset(self):
+        self._ball_handler._ball = None
