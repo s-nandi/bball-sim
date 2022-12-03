f161967f714bed7bbaf6840590cd620df5f00a26
s-nandi <saujas.nandi@gmail.com>
2022-12-02 18:07:01 -0800

    Move run to experiment

--- experiment/run.py
+++ experiment/run.py
@@ -1,13 +1,14 @@
 from bball.initiate import two_uniform_players
 from bball.strategy import DoNothing, RunToBasketAndShoot
+from bball.create import create_strategy
 from runner import run_game
 
 
 def main():
     game = two_uniform_players()
-    game.assign_team_strategy(0, RunToBasketAndShoot(5))
-    game.assign_team_strategy(1, DoNothing())
-    run_game(game, fps=30)
+    game.assign_team_strategy(0, create_strategy(5))
+    game.assign_team_strategy(1, create_strategy(3))
+    run_game(game, fps=60)
 
 
 if __name__ == "__main__":
