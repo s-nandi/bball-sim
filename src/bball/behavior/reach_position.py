a3b698cfe21a4b90e0fceaab1b4485af6ec2afc2
s-nandi <saujas.nandi@gmail.com>
2022-12-02 18:56:31 -0800

    Move utils to utils

--- src/bball/behavior/reach_position.py
+++ src/bball/behavior/reach_position.py
@@ -5,38 +5,13 @@ from bball.utils import (
     close_to,
     vector_angle_degrees,
     vector_length,
-    approx,
     difference_between,
 )
 from bball.player import Player
 from bball.behavior.reach_orientation import ReachOrientation
 from bball.behavior.stop import Stop
 from bball.behavior.scheduled_acceleration import ScheduledAcceleration
-
-
-def distance_covered(steps: int, a_max, time_frame):
-    return steps * (steps + 1) * a_max * time_frame**2
-
-
-def min_steps_needed(a_max, distance, time_frame):
-    num_steps = 0
-    while distance_covered(num_steps, a_max, time_frame) < distance:
-        num_steps += 1
-    return num_steps
-
-
-def acceleration_for(distance: float, steps: int, a_max, time_frame):
-    a_lo = 0
-    a_hi = a_max
-    while True:
-        a_mid = (a_lo + a_hi) / 2
-        possible_distance = distance_covered(steps, a_mid, time_frame)
-        if approx(possible_distance, distance):
-            return a_mid
-        if possible_distance > distance:
-            a_hi = a_mid
-        else:
-            a_lo = a_mid
+from bball.behavior.utils import acceleration_for, min_steps_needed
 
 
 @dataclass
@@ -65,7 +40,7 @@ class ReachPosition:
 
             num_steps = min_steps_needed(max_acceleration, distance, self.time_frame)
             target_acceleration = acceleration_for(
-                distance, num_steps, max_acceleration, self.time_frame
+                distance, max_acceleration, self.time_frame
             )
             self._scheduled_behavior = ScheduledAcceleration(
                 [
--- src/bball/behavior/utils.py
+++ src/bball/behavior/utils.py
@@ -35,3 +35,29 @@ def is_moving_in_orientation_direction(
         )
         assert accelerate_forward ^ accelerate_backward, f"{angle_difference}"
     return accelerate_forward
+
+
+def distance_covered(steps: int, a_max: float, time_frame: float):
+    return steps * (steps + 1) * a_max * time_frame**2
+
+
+def min_steps_needed(a_max: float, distance: float, time_frame: float):
+    num_steps = 0
+    while distance_covered(num_steps, a_max, time_frame) < distance:
+        num_steps += 1
+    return num_steps
+
+
+def acceleration_for(distance: float, a_max: float, time_frame: float):
+    steps = min_steps_needed(a_max, distance, time_frame)
+    a_lo = 0.0
+    a_hi = a_max
+    while True:
+        a_mid = (a_lo + a_hi) / 2
+        possible_distance = distance_covered(steps, a_mid, time_frame)
+        if approx(possible_distance, distance):
+            return a_mid
+        if possible_distance > distance:
+            a_hi = a_mid
+        else:
+            a_lo = a_mid
