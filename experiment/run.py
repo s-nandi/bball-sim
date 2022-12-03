a1eb1b445799fd9059972fc0098c5b65b77a047f
s-nandi <saujas.nandi@gmail.com>
2022-12-02 23:00:31 -0800

    Monitor for out of bounds players

--- experiment/run.py
+++ experiment/run.py
@@ -2,19 +2,26 @@ from bball.initiate import two_uniform_players
 from bball.strategy import DoNothing, RunToBasketAndShoot
 from bball.create import create_strategy
 from bball.game import Game
+from bball.player import Player
 from runner import run_game
 
 
 def monitor(game: Game):
-    player_1 = game.teams[0][0]
-    assert 0 <= player_1.position[0] <= game.court.width
+    threshold = 1
+
+    def check_player(player: Player):
+        assert -threshold <= player.position[0] <= game.court.width + threshold
+        assert -threshold <= player.position[1] <= game.court.height + threshold
+
+    check_player(game.teams[0][0])
+    check_player(game.teams[1][0])
 
 
 def main():
     game = two_uniform_players()
     game.assign_team_strategy(0, create_strategy(5))
-    game.assign_team_strategy(1, create_strategy(3))
-    run_game(game, fps=60, callback=lambda: monitor(game))
+    game.assign_team_strategy(1, create_strategy(10))
+    run_game(game, fps=60, monitor=lambda: monitor(game))
 
 
 if __name__ == "__main__":
