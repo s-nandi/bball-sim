7e92e404df3bf45a5cae69d4132fb2a743e95314
s-nandi <saujas.nandi@gmail.com>
2022-12-04 13:20:39 -0800

    Add missing param

--- experiment/run.py
+++ experiment/run.py
@@ -5,7 +5,7 @@ from experiment import initiate
 
 
 def main():
-    game = initiate.tests.setup_consistent_inbounds_despite_collisions(True)
+    game = initiate.tests.setup_consistent_inbounds_despite_collisions(10.0, True)
 
     monitor = Monitor(float("inf"))
     run_game(game, fps=60, display_scale=0.3, monitor=lambda: monitor.monitor(game))
