b5534d8fb34b2867d2a590051b9162a77c0b4b42
s-nandi <saujas.nandi@gmail.com>
2022-12-04 11:38:35 -0800

    Track score while monitoring

--- experiment/monitor.py
+++ experiment/monitor.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Tuple
 from time import monotonic
 from bball.game import Game
 from bball.player import Player
@@ -26,6 +26,7 @@ class Monitor:
     allowed_distance_off_court: float = 1.0
     _max_distance: float = field(init=False, default=0.0)
     _start_time: Optional[float] = field(init=False, default=None)
+    _last_score: Tuple[float, float] = field(init=False, default=(0.0, 0.0))
 
     @property
     def duration(self) -> float:
@@ -45,6 +46,10 @@ class Monitor:
         return {
             "max_distance": format_float(self.max_distance),
             "duration": format_float(self.duration),
+            "score": (
+                format_float(self._last_score[0]),
+                format_float(self._last_score[1]),
+            ),
         }
 
     def monitor(self, game: Game):
@@ -53,3 +58,4 @@ class Monitor:
         monitor_bounds(game, self.allowed_distance_off_court)
         distance = monitor_distance(game)
         self._max_distance = max(self._max_distance, distance)
+        self._last_score = game.score
