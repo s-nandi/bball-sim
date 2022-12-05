03975438d7ab8dae435397e699f52d6504eb27c0
s-nandi <saujas.nandi@gmail.com>
2022-12-05 15:07:19 -0800

    Argparse refactor

--- experiment/main.py
+++ experiment/main.py
@@ -1,40 +1,30 @@
-from experiment import initiate, run
-from bball import Game
-from bball.create import created_spaced_strategy, create_strategy
+from experiment import initiate, run, parse
 
 FPS = 90
 SPEED_SCALE = 5.0
 
 
-def visualize_game(game: Game):
-    run.visualize(
-        game,
-        display_scale=0.3,
-        fps=FPS,
-        speed_scale=SPEED_SCALE,
-    )
-
-
-def compare_strategies(game: Game):
-    run.headless(
-        game,
-        duration=10,
-        fps=FPS,
-        speed_scale=SPEED_SCALE,
-    )
-    return game.scoreboard
+def simulate(args):
+    print(args)
+    game = initiate.canonical_game(2)
+    if args.duration is not None:
+        run.headless(
+            game, fps=args.fps, speed_scale=args.speed_scale, duration=args.duration
+        )
+    else:
+        assert args.display_scale is not None
+        run.visualize(
+            game,
+            fps=args.fps,
+            speed_scale=args.speed_scale,
+            display_scale=args.display_scale,
+        )
 
 
 def main():
-    do_visualize = True
-    game = initiate.canonical_game(2)
-    game.assign_team_strategy(0, created_spaced_strategy(4, 3.0, 1.0, True, 0.5))
-    game.assign_team_strategy(1, create_strategy(10, 0.5))
-
-    if do_visualize:
-        visualize_game(game)
-    else:
-        print(compare_strategies(game))
+    args = parse.parse()
+    table = {parse.SIMULATE: simulate}
+    table[args.type](args)
 
 
 if __name__ == "__main__":
---
+++ experiment/parse.py
@@ -0,0 +1,39 @@
+import argparse
+
+SIMULATE = "simulate"
+LEARN = "learn"
+
+DURATION_SHORT = "-d"
+DURATION_LONG = "--duration"
+DISPLAY_SCALE_SHORT = "-s"
+DISPLAY_SCALE_LONG = "--display-scale"
+
+
+def _create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser("Runner")
+    subparsers = parser.add_subparsers(dest="type")
+    simulation_parser = subparsers.add_parser(SIMULATE)
+    simulation_parser.add_argument("fps", type=int)
+    simulation_parser.add_argument("speed_scale", type=float)
+    simulation_parser.add_argument(DURATION_SHORT, DURATION_LONG, type=int)
+    simulation_parser.add_argument(DISPLAY_SCALE_SHORT, DISPLAY_SCALE_LONG, type=float)
+    return parser
+
+
+def _parse_args(parser: argparse.ArgumentParser, args=None):
+    args = parser.parse_args(args)
+    headless_str = f"{DURATION_SHORT} / {DURATION_LONG} (headless)"
+    visualize_str = f"{DISPLAY_SCALE_SHORT} / {DISPLAY_SCALE_LONG} (headless)"
+    all_options_str = f"{headless_str} and {visualize_str}"
+    if args.duration is not None and args.display_scale is not None:
+        error_msg = f"{SIMULATE} cannot be used with more than one of {all_options_str}"
+        parser.error(error_msg)
+    if args.duration is None and args.display_scale is None:
+        error_msg = f"{SIMULATE} requires exactly one of {all_options_str}"
+        parser.error(error_msg)
+    return args
+
+
+def parse(args=None):
+    parser = _create_parser()
+    return _parse_args(parser, args)
