cf5b71f90bd5a5d0c6756a8f98d839f80f179e91
s-nandi <saujas.nandi@gmail.com>
2022-12-05 20:56:44 -0800

    Fix angles

--- src/bball/strategy/space_pass_shoot.py
+++ src/bball/strategy/space_pass_shoot.py
@@ -36,7 +36,11 @@ class SpacePassShoot(StrategyInterface):
         assert team_size > 0
         half_court = self._game.target_half_court(player)
 
-        angle = (player_index) * (180.0 / team_size) - 90
+        angle = 0.0
+        if team_size > 1:
+            angle = (player_index) * (180.0 / (team_size - 1)) - 90
+        else:
+            angle = 0.0
 
         offset = polar_to_cartesian(angle, self.spacing_distance)
         offset_position = sum_of(position, offset)
