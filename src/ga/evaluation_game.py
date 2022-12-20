3260ad91d8c9285a55ad79f6fab0bcdec2ecbae5
s-nandi <saujas.nandi@gmail.com>
2022-12-20 13:56:23 -0800

    Add max velocity to evaluation/canonical game

--- src/ga/evaluation_game.py
+++ src/ga/evaluation_game.py
@@ -11,9 +11,12 @@ from bball.create import (
 
 
 def evaluation_game(num_players: int) -> Game:
+    # acceleration and velocity taken from
+    # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6680831/#sec3-sports-07-00165title
     attributes = create_player_attributes(
         size=1.0,
-        max_acceleration=2.34,
+        max_velocity=3.37,  # deceleration’s start speed in Q1 (converted to m/s)
+        max_acceleration=2.34,  # minimum acceleration peak in Q3
         max_turn_degrees=480,
         velocity_decay=0.01,
         shot_probability=create_linear_shot_probability(0.8, 0.0, 0.33, 12.1),
