cba331cdf81004016783315a72180298440803dc
s-nandi <saujas.nandi@gmail.com>
2022-12-21 13:00:46 -0800

    Add basic offense (copy of movement for now)

---
+++ src/neural/basic_offense/basic_offense.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from neural.basic_offense import parse, sb3
+
+
+def main(args=None):
+    args = parse.parse(args)
+    if args.type == parse.LEARN:
+        sb3.learn(args.epochs, args.checkpoint_interval, Path(args.output_path))
+    else:
+        assert args.type == parse.LOAD
+        sb3.load(args.episodes, args.visualize, Path(args.input_path), args.epoch)
